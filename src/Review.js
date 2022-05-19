1310bebcd89b3f982fa0673cd46dd433cf0b1b9f
Jose <jlcab8@gmail.com>
2022-05-19 17:50:00 +0200

    prev & next working. Check index number to avoid crash

--- src/Review.js
+++ src/Review.js
@@ -1,9 +1,57 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+
+    if (number < 0) {
+      return people.length - 1;
+    }
+
+    return number;
+  };
+
+  const nextReview = () => {
+    setIndex((prevState) => {
+      return checkNumber(prevState + 1);
+    });
+  };
+
+  const prevReview = () => {
+    setIndex((prevState) => {
+      return checkNumber(prevState - 1);
+    });
+  };
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button onClick={prevReview} className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button onClick={nextReview} className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">surprise me</button>
+    </article>
+  );
 };
 
 export default Review;
